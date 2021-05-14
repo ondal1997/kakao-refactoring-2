9abc397dd2117e1703b184f8f272f5bea093a94e
eojin-dev <eojin-dev@naver.com>
2021-05-14 22:01:55 +0900

    OrderForm 진행 중

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -1,3 +1,14 @@
 function OrderForm() {
-  return "";
+  const { options, subOptions, basket } = state;
+
+  const Items = () =>
+    basket
+      .map(({ subOptionId, size }) => {
+        const subOption = subOptions.find(({ id }) => id === subOptionId);
+        const option = options.find(({ id }) => id === subOption.parentOptionId);
+        return `<p>${option.optionName} ${subOption.optionName}</p>`;
+      })
+      .join("");
+
+  return Items();
 }
